b10b29d66525d6665c4601448d45c7910484a632
kirsansan <kirill_home@mail.ru>
2023-03-26 17:26:39 +0300

    add calling delta function in main, add .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from funcs import summ_two_digit
+from funcs import summ_two_digit, delta_two_digits
 
 
 def main():
     summ_two_digit(1, 5)
+    delta_two_digits(20, 21)
 
 
 if __name__ == '__main__':
