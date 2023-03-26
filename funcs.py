b8f351431325af747376dde8e620c074189958ac
kirsansan <kirill_home@mail.ru>
2023-03-26 17:24:02 +0300

    add  main with summarize

--- funcs.py
+++ funcs.py
@@ -1,2 +1,6 @@
 def summ_two_digit(digit1, digit2):
     return digit1 + digit2
+
+def delta_two_digits(digit1, digit2):
+    return digit1 - digit2
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from funcs import summ_two_digit
+
+
+def main():
+    summ_two_digit(1, 5)
+
+
+if __name__ == '__main__':
+    main()
